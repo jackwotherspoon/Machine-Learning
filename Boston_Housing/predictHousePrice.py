fe72436e7c7b690d9bdec1a2c56182b16ab6c372
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-07-22 23:20:52 -0400

    Moving to sklearn instead of keras.

--- Boston_Housing/predictHousePrice.py
+++ Boston_Housing/predictHousePrice.py
@@ -1,6 +1,15 @@
 # Import dependencies
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_boston
 
-# Load the Boston Housing dataset
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.boston_housing.load_data()
+# load the boston housing dataset
+boston = load_boston()  
+print(boston.keys())
+# seperate data into its features and labels 
+features = pd.DataFrame(np.array(boston["data"]), columns = boston["feature_names"])
+print(features.head())
+features.shape()
+print(test)
