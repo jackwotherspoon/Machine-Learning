6fbc7e4446c124144f1a18196e4327cc1ff8852a
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-07-23 18:26:01 -0400

    Changing variable names and adding labels to dataframe.

--- Boston_Housing/predictHousePrice.py
+++ Boston_Housing/predictHousePrice.py
@@ -6,10 +6,14 @@ import numpy as np
 from sklearn.datasets import load_boston
 
 # load the boston housing dataset
-boston = load_boston()  
-print(boston.keys())
-# seperate data into its features and labels 
-features = pd.DataFrame(np.array(boston["data"]), columns = boston["feature_names"])
-print(features.head())
-features.shape()
-print(test)
+boston_dataset = load_boston()  
+print("All the different keys in dataset are :", boston_dataset.keys())
+
+# seperate data into its features 
+boston = pd.DataFrame(np.array(boston_dataset["data"]), columns = boston_dataset["feature_names"])
+print("Snapshot of what dataframe looks like :\n", boston.head())
+print("Shape of the datasets inputs (features) : ", boston.shape)
+
+# add median value of homes (labels) to dataframe
+boston['MEDV'] = boston_dataset["target"]
+print("Snapshot of dataframe with target values MEDV now shown :\n", boston.head())
