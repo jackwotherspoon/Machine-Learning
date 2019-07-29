627a95228eefe0c913484c1e7470d20fd42d9abc
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-07-28 21:47:45 -0400

    Splitting data into training and test set.

--- Boston_Housing/predictHousePrice.py
+++ Boston_Housing/predictHousePrice.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
 import seaborn as sns
 
 # load the boston housing dataset
@@ -25,4 +26,18 @@ print("Checking if any values are missing in dataset:\n", boston.isnull().sum())
 # plot median value of houses using seaborn
 sns.set(rc={'figure.figsize':(11.7,8.27)})
 sns.distplot(boston['MEDV'], bins=30)
-plt.show()
+# plt.show()
+
+# create correlation matrix to measure relationship between variables
+correlation_matrix = boston.corr().round(2)
+sns.heatmap(data = correlation_matrix, annot = True)
+#plt.show()
+
+# split data into training set and test set
+features = boston.drop('MEDV', axis = 1)
+labels   = boston['MEDV']
+X_train, X_test, Y_train, Y_test = train_test_split(features, labels, test_size = 0.3, random_state = 5)
+print(X_train.shape)
+print(X_test.shape)
+print(Y_train.shape)
+print(Y_test.shape)
