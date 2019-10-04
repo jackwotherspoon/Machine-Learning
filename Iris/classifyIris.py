e9fda9467555343572566e832ca17dc728dd310e
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-10-03 21:18:04 -0400

    Load iris dataset and create jupyter notebook.

---
+++ Iris/classifyIris.py
@@ -0,0 +1,9 @@
+# import dependencies
+from sklearn import datasets
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+# load iris dataset
+iris = datasets.load_iris()
